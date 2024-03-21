412d0bf9da1b291374025f326dc36411dcdea212
jacinta <jacintawmwangi21.com>
2024-03-21 22:01:49 +0300

    learned classes

---
+++ classes.rb
@@ -0,0 +1,11 @@
+#everything in ruby is an object and each object has a class and a method
+puts "Hello world".class #=> String
+puts 7.class
+puts 6.9765.class
+puts ({:day => "monday", :menu => "chef's special"}).class
+puts :my_type.class
+puts true.class
+puts nil.class
+puts false.class
+puts [5, "kill", 6.978, :symbol].class
+#an instance of a class is an object that belongs to that class
