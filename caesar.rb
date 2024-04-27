788faa4857c8b2598b2989c3c1b0d4bf1d76e400
jacinta <jacintawmwangi21.com>
2024-04-27 12:41:21 +0300

    code challenge

--- caesar.rb
+++ caesar.rb
@@ -23,9 +23,23 @@ class Caesar
     end
 
     def decrypt(word)
+        decrypt = ""
+        word.split("").to_a.each do |letter|
+            if @@Upcase.include? letter
+                decrypt += @@Upcase[(@@Upcase.index(letter)-key) % 26]
+            elsif @@Downcase.include? letter
+            decrypt << @@Downcase[(@@Downcase.index(letter)-key) % 26]
+            else
+                decrypt << letter
+            end
+        end
+        decrypt
     end
 
 end
 
 c = Caesar.new(2)
 puts c.encrypt("hello HAPPY world 2024 ")
+
+
+puts c.decrypt("jgnnq JCRRA yqtnf 2024")
